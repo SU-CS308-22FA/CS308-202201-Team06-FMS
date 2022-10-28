8d15b2d5d86a5ac84f27f133ad0272604e245594
zgr2788 <ozgur.beker@gmail.com>
2022-10-28 13:21:08 +0300

    finalized initial schemas and models for backend

--- backend/models.py
+++ backend/models.py
@@ -47,13 +47,13 @@ class Admin(_database.Base):
     __tablename__ = "admins"
 
     # Cols
-    id = _sql.Column(_sql.Integer, primary_key = True, index = True)  # Unique admin id - pkey
-    email = _sql.Column(_sql.String, unique = True, index = True)     # E-mail for admin
-    ssn = _sql.Column(_sql.Integer, Unique = True, index = True)      # SSN (SGK) No.
-    pass_hash = _sql.Column(_sql.String)
-    first_name = _sql.Column(_sql.String, index = True)
-    last_name = _sql.Column(_sql.String, index = True)
-    date_registered = _sql.Column(_sql.DateTime, default = _dt.datetime.utcnow().replace(tzinfo=from_zone).astimezone(to_zone))
+    id = _sql.Column(_sql.Integer, primary_key = True, index = True)                                                             # Unique admin id - pkey
+    email = _sql.Column(_sql.String, unique = True, index = True)                                                                # E-mail for admin
+    ssn = _sql.Column(_sql.Integer, Unique = True, index = True)                                                                 # SSN (SGK) No.
+    pass_hash = _sql.Column(_sql.String)                                                                                         # Password hash
+    first_name = _sql.Column(_sql.String, index = True)                                                                          # Name
+    last_name = _sql.Column(_sql.String, index = True)                                                                           # Surname
+    date_registered = _sql.Column(_sql.DateTime, default = _dt.datetime.utcnow().replace(tzinfo=from_zone).astimezone(to_zone))  # Date of entry creation
 
     def verify_password(self, password: str):
         return _hash.bcrypt.verify(password, self.pass_hash)
@@ -64,9 +64,11 @@ class BudgetItem(_database.Base):
     __tablename__ = "budgetitems"
 
     # Cols
-    team_name = _sql.Column(_sql.String, _sql.ForeignKey("teams.name"), primary_key = True)
-    item_name = _sql.Column(_sql.String, primary_key = True, index = True)
-    amount = _sql.Column(_sql.Float)
+    team_name = _sql.Column(_sql.String, _sql.ForeignKey("teams.name"), primary_key = True)                                       # Team name
+    item_name = _sql.Column(_sql.String, primary_key = True, index = True)                                                        # Budget item name
+    amount = _sql.Column(_sql.Float)                                                                                              # Amount of the item
+    date_created = _sql.Column(_sql.DateTime, default = _dt.datetime.utcnow().replace(tzinfo=from_zone).astimezone(to_zone))      # Date of entry creation
+    date_last_updated = _sql.Column(_sql.DateTime, default = _dt.datetime.utcnow().replace(tzinfo=from_zone).astimezone(to_zone)) # Date of last entry update
 
     # Establish relation with TeamAcc
     teams = _orm.relationship("Team", back_populates = "owner")
---
+++ backend/schemas.py
@@ -0,0 +1,79 @@
+# Models file for database Team 06 - FMS
+#
+# @zgr2788
+#
+# Description:
+# Includes the schemas to the tables used in backend.
+# For further description on variables, refer to `models.py`
+
+import datetime as _dt
+import pydantic as _pydantic
+
+
+# Team Schemas
+## Base model
+class _TeamBase(_pydantic.BaseModel):
+    email: str
+    name: str
+
+## Team create model - inherits TeamBase
+class TeamCreate(_TeamBase):
+    pass_hash: str
+
+
+    class Config:
+        orm_mode = True
+
+## Main Team schema - inherits TeamBase
+class Team(_TeamBase):
+    id: int
+    budget_total: float
+    budget_alloc: float
+    budget_rem: float
+    is_overlimit: bool
+
+    class Config:
+        orm_mode = True
+
+
+# Admin Schemas
+## Base model
+class _AdminBase(_pydantic.BaseModel):
+    email: str
+    ssn: int
+    first_name: str
+    last_name: str
+
+## Admin create model - inherits AdminBase
+class AdminCreate(_AdminBase):
+    pass_hash: str
+
+    class Config:
+        orm_mode = True
+
+## Main Admin schema - inherits AdminBase
+class Admin(_AdminBase):
+    id: int
+    date_registered: _dt.datetime
+
+    class Config:
+        orm_mode = True
+
+
+# Budget Schemas
+## Base model
+class _BudgetItemBase(_pydantic.BaseModel):
+    team_name: str
+    item_name: str
+    amount: float
+
+# Budget create model - inherits BudgetItemBase
+class BudgetItemCreate(_BudgetItemBase):
+    pass
+
+class BudgetItem(_BudgetItemBase):
+    date_created: _dt.datetime
+    date_last_updated: _dt.datetime
+
+    class Config:
+        orm_mode = True
