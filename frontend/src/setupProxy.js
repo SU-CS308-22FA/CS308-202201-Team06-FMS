7584edf15f1efcdbf425ea63804e79279ab333ac
zgr2788 <ozgur.beker@gmail.com>
2022-10-30 19:04:10 +0000

    Fixed proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://finms-backend.herokuapp.com/',
+      target: 'https://finms-backend.herokuapp.com',
       changeOrigin: true,
     })
   );
