7e1f4984b4421b62f1ac15871471bb98390b3b77
Seckin Karabacakoglu <83659989+kaanska1@users.noreply.github.com>
2022-11-20 23:12:00 +0300

    Initialized balance design

--- frontend/src/components/TeamBalance.jsx
+++ frontend/src/components/TeamBalance.jsx
@@ -5,6 +5,20 @@ import { useState, useEffect } from "react";
 
 
 const TeamBalance = ({ loggedInTeam }) => {
-    
+    const [teamToken, setTeamToken, teamLogin, setTeamLogin, teamName, setTeamName, total, setTotal, rem, setRem, alloc, setAlloc] = useContext(TeamContext);
+
+    if (teamLogin)
+        return (
+            <div>
+                Total : {total}
+                Allocated : {alloc}
+                Remaining : {rem} 
+            
+
+            </div>
+
+        );
+
+    return (null);
 
 }
