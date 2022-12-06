c8f6eff0a9289f0324c053de02779e3062321872
Özgür Yılmaz Beker <89029806+zgr2788@users.noreply.github.com>
2022-12-06 18:22:42 +0000

    Update setupProxy.js
    Revert proxy change

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api/',
     createProxyMiddleware({
-      target: "http://127.0.0.1:8000",
+      target: "https://finms-backend.onrender.com/",
       changeOrigin: true,
     })
   );
