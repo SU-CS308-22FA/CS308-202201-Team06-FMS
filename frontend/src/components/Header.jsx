835c9c3f78b669542b5a8dd82195db90e8196338
Yavuz Mehmet Oguz Simsek <simsekyavuz@atlassian.com>
2023-01-03 02:59:31 +0300

    Add upload image feature

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,8 @@
 //
 // @zgr2788
 import React, { useContext } from "react";
+import { useState } from "react";
+import axios from 'axios';
 
 import { AdminContext } from "../context/AdminContext";
 import { TeamContext } from "../context/TeamContext";
@@ -15,9 +17,78 @@ import Button from "@mui/material/Button";
 
 
 const Header = () => {
-    const [teamToken, setTeamToken, teamLogin, setTeamLogin, teamName, setTeamName] = useContext(TeamContext);
     const [adminToken, setAdminToken, adminLogin, setAdminLogin] = useContext(AdminContext);
 
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [errorMessage, setErrorMessage] = useState("");
+    const [teamToken, setTeamToken, teamLogin, setTeamLogin, teamName, setTeamName] = useContext(TeamContext);
+
+
+    const onFileChange = event => {
+     
+        // Update the state
+        setSelectedFile(event.target.files[0]);
+      }
+
+    const onFileUpload = async () => {
+     
+        // Create an object of formData
+        const formData = new FormData();
+       
+        // Update the formData object
+        formData.append(
+          "myFile",
+          selectedFile,
+          selectedFile.name
+        );
+       
+        const requestOptions = {
+            method: "POST",
+            headers: {
+                Authorization: "Bearer " + adminToken,
+            },
+
+            body: formData
+        };
+        // Details of the uploaded file
+        console.log(selectedFile);
+       
+        // Request made to the backend api
+        // Send formData object
+        const response = await fetch("/api/admins/profilepics/" + "a@a", requestOptions);
+        const data = await response.json();
+
+        if (!response.ok) {
+            setErrorMessage(data.detail);
+        } else {
+            document.getElementById("fileUpload").value = "";
+        }
+      }
+
+    const fileData = () => {
+     
+        if (selectedFile) {
+            
+          return (
+            <div>
+              <h2>File Details:</h2>
+              <p>File Name: {selectedFile.name}</p>
+    
+              <p>File Type: {selectedFile.type}</p>
+    
+    
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              <br />
+              <h4>Choose before Pressing the Upload button</h4>
+            </div>
+          );
+        }
+      }
+      
     const handleLogout = () => {
         setAdminToken(null);
         setTeamToken(null);
@@ -28,25 +99,18 @@ const Header = () => {
 
     if (adminLogin || teamLogin)
         return (
-            /*<div>
-                <div className="has-text-centered m-6">
-                    <button className="button" onClick={handleLogout}>
-                        Logout
-                    </button>
-                </div>
-
-            </div>*/
             <AppBar position="fixed">
-        <Toolbar>
-          {/*Inside the IconButton, we 
-           can render various icons*/}
-
-          {/* The Typography component applies 
-           default font weights and sizes */}
-  
+        <Toolbar>  
           <Typography variant="h6" 
             component="div" sx={{ flexGrow: 1 }}>
             GeeksforGeeks Header
+
+            <input type="file" onChange={onFileChange} />
+            <button onClick={onFileUpload}>
+                  Upload!
+                </button>
+                {fileData()}
+            <img src="https://www.w3schools.com/images/lamp.jpg"/>
           </Typography>
           <button className="button" onClick={handleLogout}>
                         Logout
