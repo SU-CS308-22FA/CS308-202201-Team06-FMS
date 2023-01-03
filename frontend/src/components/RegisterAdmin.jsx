63738631dc5b0da1f38ce61f419fd6eb2e289dda
Ege Bağırsakçı <egebagirsakci@sabanciuniv.edu>
2023-01-03 15:54:12 +0300

    Added register modal

--- frontend/src/components/RegisterAdmin.jsx
+++ frontend/src/components/RegisterAdmin.jsx
@@ -8,7 +8,7 @@ import ErrorMessage from "./ErrorMessage";
 import SuccessMessage from "./SuccessMessage";
 import { AdminContext } from "../context/AdminContext";
 
-const RegisterAdmin = ({loggedInAdmin}) => {
+const RegisterAdmin = ({ loggedInAdmin, active, handleModal }) => {
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [surname, setSurname] = useState("");
@@ -24,13 +24,13 @@ const RegisterAdmin = ({loggedInAdmin}) => {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({email: email, first_name: name, last_name: surname, pass_hash: password})
+            body: JSON.stringify({ email: email, first_name: name, last_name: surname, pass_hash: password })
         };
 
         const response = await fetch("/api/admins", requestOptions);
         const data = await response.json();
 
-        if (!response.ok){
+        if (!response.ok) {
             setErrorMessage(data.detail);
             setSuccessMessage("");
         } else {
@@ -41,7 +41,7 @@ const RegisterAdmin = ({loggedInAdmin}) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if (password === confirmationPassword && password.length > 8){
+        if (password === confirmationPassword && password.length > 8) {
             submitRegistration();
         } else {
             setErrorMessage("Ensure that the passwords are matching and longer than 8 characters.");
@@ -51,99 +51,102 @@ const RegisterAdmin = ({loggedInAdmin}) => {
 
 
 
-   
-    if (loggedInAdmin){
+
+    if (loggedInAdmin) {
         return (
-        <div className = "column">
-            <form className = "box" onSubmit = {handleSubmit}>
-                <h1 className = "title has-text-centered">Register New Admin</h1>
-                
-                <div className = "field">
-                    <label className = "label">Email Address</label>
-                    <div className="control">
-                        <input 
-                            type="email" 
-                            placeholder = "E-mail" 
-                            value = {email} 
-                            onChange = { (e) => setEmail(e.target.value) }
-                            className = "input"
-                            required                        
-                        />
-                    </div>
+            <div className={`modal ${active && "is-active"}`}>
+                <div className="modal-background" onClick={handleModal}></div>
+
+                <div className="column">
+                    <form className="box" onSubmit={handleSubmit}>
+                        <h1 className="title has-text-centered">Register New Admin</h1>
+
+                        <div className="field">
+                            <label className="label">Email Address</label>
+                            <div className="control">
+                                <input
+                                    type="email"
+                                    placeholder="E-mail"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="input"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Name</label>
+                            <div className="control">
+                                <input
+                                    type="text"
+                                    placeholder="First Name"
+                                    value={name}
+                                    onChange={(n) => setName(n.target.value)}
+                                    className="input"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Surname</label>
+                            <div className="control">
+                                <input
+                                    type="text"
+                                    placeholder="Surname"
+                                    value={surname}
+                                    onChange={(s) => setSurname(s.target.value)}
+                                    className="input"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <br />
+                        <br />
+
+                        <div className="field">
+                            <label className="label">Password</label>
+                            <div className="control">
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    value={password}
+                                    onChange={(p) => setPassword(p.target.value)}
+                                    className="input"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <div className="field">
+                            <label className="label">Confirm Password</label>
+                            <div className="control">
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    value={confirmationPassword}
+                                    onChange={(c) => setConfirmationPassword(c.target.value)}
+                                    className="input"
+                                    required
+                                />
+                            </div>
+                        </div>
+
+                        <ErrorMessage message={errorMessage} />
+                        <SuccessMessage message={successMessage} />
+                        <br />
+
+                        <button className="button is-primary" type="submit">
+                            Register
+                        </button>
+
+                    </form>
                 </div>
+            </div>
 
-                <div className = "field">
-                    <label className = "label">Name</label>
-                    <div className="control">
-                        <input 
-                            type="text" 
-                            placeholder = "First Name" 
-                            value = {name} 
-                            onChange = { (n) => setName(n.target.value) }
-                            className = "input"
-                            required                        
-                        />
-                    </div>
-                </div>
-
-                <div className = "field">
-                    <label className = "label">Surname</label>
-                    <div className="control">
-                        <input 
-                            type="text" 
-                            placeholder = "Surname" 
-                            value = {surname} 
-                            onChange = { (s) => setSurname(s.target.value) }
-                            className = "input"
-                            required                        
-                        />
-                    </div>
-                </div>
-                
-                <br />
-                <br />
-
-                <div className = "field">
-                    <label className = "label">Password</label>
-                    <div className="control">
-                        <input 
-                            type="password" 
-                            placeholder = "Password" 
-                            value = {password} 
-                            onChange = { (p) => setPassword(p.target.value) }
-                            className = "input"
-                            required                        
-                        />
-                    </div>
-                </div>
-
-                <div className = "field">
-                    <label className = "label">Confirm Password</label>
-                    <div className="control">
-                        <input 
-                            type="password" 
-                            placeholder = "Password" 
-                            value = {confirmationPassword} 
-                            onChange = { (c) => setConfirmationPassword(c.target.value) }
-                            className = "input"
-                            required                        
-                        />
-                    </div>
-                </div>
-
-                <ErrorMessage message = {errorMessage} />
-                <SuccessMessage message = {successMessage} />
-                <br />
-
-                <button className="button is-primary" type = "submit">
-                    Register
-                </button>
-
-            </form>
-        </div>
-
-
-    );
+        );
     }
 
     return (null);
